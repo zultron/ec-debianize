9d0ad005ca0543e07ba617fcf21df087234effc8
John Morris <john@zultron.com>
2020-08-18 20:57:06 +0000

    get_source.sh:  Add command-line args to control actions
    The `get_source.sh` script has multiple functions:  Clone upstream
    repo and apply patches; create a source archive for packaging; and
    extracting source into the source tree.
    
    These functions may now be run individually with command-line options
    to the script.  This makes it easier to go through iterations in a dev
    environment, where for example repeated cloning takes time and is
    unnecessary.
    
    The new `-C` option cleans unpacked sources out of the
    `etherlabmaster` directory.
    
    The old `cleanup` function that removed the clones is now removed.

--- get_source.sh
+++ get_source.sh
@@ -10,26 +10,75 @@ HG_DIR="etherlabmaster-hg"
 PATCH_REPOS="http://hg.code.sf.net/u/uecasm/etherlab-patches"
 PATCH_COMMIT=`(echo "$DEB_VERSION" | sed -rne 's/^[0-9.]+\+[0-9]*hg([0-9a-z]*)p([0-9a-z]*).*$$/\2/p')`
 
-function cleanup {
-  rm -rf "$HG_DIR"
+usage() {
+    if test -z "$*"; then
+	RC=0
+    else
+	echo "Error:  $*" >&2
+	RC=1
+    fi
+    cat >&2 <<EOF
+Usage: $0 [-C] [-c] [-x] [-a]
+    -C:  Clean up "etherlabmaster" directory
+    -c:  Clone upscream repositories and apply patchsets
+    -x:  Extract sources into "etherlabmaster" directory & create source tarball
+    -a:  All (except clone); like "-Ccx"
+    -h:  This help message
+EOF
+    exit $RC
 }
-trap cleanup EXIT
+
+CLEAN=false
+CLONE=false
+EXTRACT=false
+while getopts aCcxh ARG; do
+    case $ARG in
+	a) CLONE=true; CLEAN=true; EXTRACT=true ;;
+	C) CLEAN=true ;;
+	c) CLONE=true ;;
+	x) EXTRACT=true ;;
+	h) usage ;;
+	*) usage "Illegal option -$OPTARG" ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+if test "${CLEAN}${CLONE}${EXTRACT}" = "falsefalsefalse"; then
+    usage "Please select a argument"
+fi
+
+# Be sure we're in the right directory
+cd $(dirname $0)
+
 set -e
 
-echo "Cloning HG commit $HG_COMMIT..."
-hg clone $HG_REPOS "$HG_DIR" -r $HG_COMMIT
+if $CLEAN; then
+    echo 'Cleaning up "etherlabmaster" directory'
+    git clean -Xdf etherlabmaster
+fi
 
-echo "Cloning patchset commit $PATCH_COMMIT..."
-hg clone $PATCH_REPOS "$HG_DIR/.hg/patches" -r $PATCH_COMMIT
+if $CLONE && test ! -d $HG_DIR; then
+    echo "Cloning HG commit $HG_COMMIT..."
+    hg clone $HG_REPOS "$HG_DIR" -r $HG_COMMIT
 
-echo "Apply patchset..."
-(cd $HG_DIR && hg qpush -a)
+    echo "Cloning patchset commit $PATCH_COMMIT..."
+    hg clone $PATCH_REPOS "$HG_DIR/.hg/patches" -r $PATCH_COMMIT
 
-echo "Create source archive..."
-(cd $HG_DIR && hg archive "../$DEB_ORIG")
+    if test -f $HG_DIR/.hg/hgrc && ! grep -q '^mq' $HG_DIR/.hg/hgrc; then
+	echo "Enabling hg mq extension..."
+	echo -e "[extensions]\nmq =" >> $HG_DIR/.hg/hgrc
+    fi
 
-echo "Extracting source..."
-(cd etherlabmaster && tar xfz "../$DEB_ORIG" --strip 1)
+    echo "Apply patchset..."
+    (cd $HG_DIR && hg qpush -a)
+fi
+
+if $EXTRACT; then
+    echo "Create source archive..."
+    (cd $HG_DIR && hg archive "../$DEB_ORIG")
+
+    echo "Extracting source..."
+    (cd etherlabmaster && tar xfz "../$DEB_ORIG" --strip 1)
+fi
 
 echo "DONE."
-
